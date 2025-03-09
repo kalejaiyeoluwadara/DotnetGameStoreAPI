6b0b36642f50008569154a4189788086bbaf37c1
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-03-09 23:04:49 +0100

    added giignore

--- GameStore/obj/Debug/net9.0/GameStore.AssemblyInfo.cs
+++ GameStore/obj/Debug/net9.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c1825e210b51a854de40c3f18849a5ccf2824b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00d15958963d8891e12cc7a1af6df1c0feee7e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
